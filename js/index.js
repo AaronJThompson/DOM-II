ed9be80f9ceb542254c7b7cec8ddf6712f8111ca
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-14 13:36:45 +0100

    Close nav bar if scroll quickly

--- js/index.js
+++ js/index.js
@@ -7,9 +7,19 @@ let lastTime = new Date().getTime();
 window.addEventListener("scroll", e => {
     let timeDelta = e.timeStamp - lastTime;
     let scrollDelta = window.scrollY - topScroll;
-    let speed = scrollDelta/timeDelta;
-    console.log("Scroll speed:", speed * (10 ** 12));
+    let speed = (scrollDelta/timeDelta) * (10 ** 12);
     topScroll = window.scrollY;
+    if (window.scrollY > navHeader.clientHeight){
+        if (speed < -40){
+            navHeader.classList.remove("fadeIn");
+            navHeader.classList.add("hidden");
+        }
+        if (speed > 30){
+            navHeader.classList.replace("hidden", "fadeIn");
+        }
+    } else {
+        navHeader.classList.replace("hidden", "fadeIn");
+    }
 })
 
 window.addEventListener("load", event => body.classList.replace("hidden", "fadeIn"));
