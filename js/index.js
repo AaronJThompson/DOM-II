23c375f9b844a685c8d3399b1ea369c4e7554ccd
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-14 13:28:49 +0100

    Add code to calculate scroll speed

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 // Your code goes here
 const body = document.body;
+const navHeader = body.querySelector(".main-navigation");
+
+let topScroll = window.scrollY;
+let lastTime = new Date().getTime();
+window.addEventListener("scroll", e => {
+    let timeDelta = e.timeStamp - lastTime;
+    let scrollDelta = window.scrollY - topScroll;
+    let speed = scrollDelta/timeDelta;
+    console.log("Scroll speed:", speed * (10 ** 12));
+    topScroll = window.scrollY;
+})
 
 window.addEventListener("load", event => body.classList.replace("hidden", "fadeIn"));
